f2b612bb29f89cb7638b75e244e7f0f39274a8a9
Raphael Oliveira <raphaelbrf@gmail.com>
2020-06-11 17:36:55 -0300

    .

---
+++ _tests/import_test.py
@@ -0,0 +1,5 @@
+from test import divide
+
+print(
+    divide(12.3, 6.0)
+)
---
+++ _tests/test.py
@@ -0,0 +1,6 @@
+def divide(dividend: int ,divisor: int) -> int:
+    return dividend / divisor
+
+print('my_module', __name__)
+
+print(divide(12.3, 4.7))
