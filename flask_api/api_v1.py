b0cc8c3af759f62230b8f11fde773cea8bc5e314
Raphael Oliveira <raphaelbrf@gmail.com>
2020-06-17 10:57:49 -0300

    - mudança na estrutura do código - método get e post da api de lojas completo

--- flask_api/api_v1.py
+++ flask_api/api_v1.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask.views import MethodView
+
 app = Flask(__name__)
 
 if __name__ == "__main__": app.run(debug=True)
@@ -21,15 +23,45 @@ stores = [
 ]
 
 
-@app.route('/stores/', methods=['GET', 'POST'])
-@app.route('/stores/<name>/', methods=['GET'])
-def get_or_create_stores(name=None):
-    
-    return f'{stores} {name}'
+class StoresAPI(MethodView):
+
+    def get(self, name=None):
+        if name is None:
+            return jsonify({'stores': stores})
+        else:
+            for store in stores:
+                if name == store['name']:
+                    return jsonify(store)
+                else:
+                    return jsonify({'error': 'store not found'})
+
+    def post(self):
+        request_data = request.get_json()
+        
+        new_store = {
+            "name": request_data['name'],
+            "items": []
+        }
+        stores.append(new_store)
+
+        return jsonify(new_store)
+
+stores_view = StoresAPI.as_view('stores')
+app.add_url_rule('/stores/', view_func=stores_view, methods=['GET', 'POST'])
+app.add_url_rule('/stores/<name>/', view_func=stores_view, methods=['GET'])
 
 
-@app.route('/stores/<name>/items/', methods=['GET', 'POST'])
-@app.route('/stores/<name>/items/<item>/', methods=['GET'])
-def get_or_create_items(name, item=None):
+class StoresItemsAPI(MethodView):
+
+    def get(self, name, item=None):
+        if item is None:
+            return 'list of items'
+        else:
+            return 'one items'
     
-    return 'get_or_create_items'
+    def post(self, name):
+        return 'create one item'
+
+stores_items_view = StoresItemsAPI.as_view('items')
+app.add_url_rule('/stores/<name>/items/', view_func=stores_items_view, methods=['GET', 'POST'])
+app.add_url_rule('/stores/<name>/items/<item>/', view_func=stores_items_view, methods=['GET'])
