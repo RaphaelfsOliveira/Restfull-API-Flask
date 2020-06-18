483b539e9ce314c60e7ed16088828404e0532c38
Raphael Oliveira <raphaelbrf@gmail.com>
2020-06-18 13:41:30 -0300

    - implementando count e delete

--- flask_rest/app.py
+++ flask_rest/app.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
+
+import datetime
 
 app = Flask(__name__)
 api = Api(app)
@@ -13,30 +15,59 @@ def home():
 
 items = []
 
-class Items(Resource):
+class Item(Resource):
     
     def get(self, name=None):
-        if name is None:
-            return {'items': 'list of items'}
-        
         for item in items:
             if item['name'] == name:
                 return item
         
         return {'message': 'item not found'}, 404
     
-    def post(self, name, price=0):
-        if name:
+    def delete(self, name):
+        for item in items:
+            if item['name'] == name:
+                items.remove(item)
+                return {'message': 'item removed'}
+        
+        return {'message': 'item not found'}, 404
+
+
+class ItemListCreateUpdate(Resource):
+    
+    def get(self):
+        return {
+            'items': items,
+            'count': len(items)
+        }
+    
+    def post(self):
+        data = request.get_json()
+        if data:
             new_item = {
-                'name': name,
-                'price': price
+                'name': data['name'],
+                'price': data['price']
             }
             items.append(new_item)
             
             return new_item, 201
         
-        return {'message': 'item need name'}, 500
-        
+        return {'message': 'item need some name and price'}, 500
+    
+    def put(self):
+        data = request.get_json()
+        if data:
+            for item in items:
+                if data['name'] == item['name']:
+                    item.update(data)
+                    return data
+                
+            items.append(data)
+            return data, 201
+
+        return {'message': 'item need some name and price'}, 500
 
 
-api.add_resource(Items, '/items/<string:name>')
+api.add_resource(ItemListCreateUpdate, '/items')
+api.add_resource(Item, '/items/<string:name>')
+
