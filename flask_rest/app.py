ece0332bd3e1681557cb3d7d2ba3599183cfbbf5
Raphael Oliveira <raphaelbrf@gmail.com>
2020-06-18 15:52:05 -0300

    - refatorando algumas funções

--- flask_rest/app.py
+++ flask_rest/app.py
@@ -25,10 +25,10 @@ class Item(Resource):
         return {'message': 'item not found'}, 404
     
     def delete(self, name):
-        for item in items:
-            if item['name'] == name:
-                items.remove(item)
-                return {'message': 'item removed'}
+        item = next(filter(lambda key: key['name'] == name, items), None)
+        if item:
+            items.remove(item)
+            return {'message': 'item removed'}
         
         return {'message': 'item not found'}, 404
 
@@ -47,12 +47,10 @@ class ItemListCreateUpdate(Resource):
             if next(filter(lambda key: key['name'] == data['name'], items), None):
                 return {'message': 'An item with name {} already exists'.format(data['name'])}, 400
 
-            new_item = {
+            items.append({
                 'name': data['name'],
                 'price': data['price']
-            }
-            items.append(new_item)
-            
+            })
             return new_item, 201
         
         return {'message': 'item need some name and price'}, 500
