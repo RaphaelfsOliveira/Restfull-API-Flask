49e9678088176e0d0d6442819bcf96150b801e83
Raphael Oliveira <raphaelbrf@gmail.com>
2020-06-19 20:24:04 -0300

    - conexão com o banco de dados

--- flask_rest/security.py
+++ flask_rest/security.py
@@ -1,14 +1,9 @@
 from werkzeug.security import safe_str_cmp
 from user import User
 
-users = [User(1, 'Rolf', '1234')]
-
-username_mapping = {user.username: user for user in users}
-userid_mapping = {user.id: user for user in users}
 
 def authenticate(username, password):
-    print(username_mapping)
-    user = username_mapping.get(username, None)
+    user = User.find_by_username(username)
     if user and safe_str_cmp(user.password, password):
         return user
     
@@ -17,6 +12,6 @@ def authenticate(username, password):
 def identity(payload):
     user_id = payload['identity']
     if user_id:
-        return userid_mapping.get(user_id, None)
+        return User.find_by_id(user_id)
     
     return {'message': 'user not found'}, 404
